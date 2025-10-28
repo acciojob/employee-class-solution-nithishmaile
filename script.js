e1defde607adce2281ee6ef20a9b42cbde334248
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2025-10-28 12:51:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function Employee(name, salary) {
+/*function Employee(name, salary) {
   this.name = name;
   this.salary = salary;
 }
@@ -18,4 +18,29 @@ Manager.prototype.constructor = Manager;
 Manager.prototype.giveRaise = function(amount) {
   Employee.prototype.giveRaise.call(this, amount);
   console.log(`New salary for ${this.name} in ${this.department}: ${this.salary}`);
+}*/
+
+class Employee{
+	constructor(name,salary){
+		this.name=name;
+		this.salary=salary
+	}
+
+	giveRaise(amount){
+		this.salary=salary+amount;
+	}
 }
+
+class Manager extends Employee{
+	constructor(name,salary,department){
+		super(name,salary)
+		this.department=department
+	}
+}
+
+let employee = new Employee("John Doe", 50000);
+employee.giveRaise(5000);
+console.log(employee.salary); // 55000
+
+let manager = new Manager("Jane Smith", 80000, "Sales");
+manager.giveRaise(10000); // New salary for Jane Smith in Sales: 90000
